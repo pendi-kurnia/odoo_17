0acf5e085a6d45e5843b60965e42aa04754f8a98
pendi-kurnia <emailkamu@example.com>
2025-11-12 14:31:03 +0000

    menambah record kanban

--- addons/kampus/models/mahasiswa.py
+++ addons/kampus/models/mahasiswa.py
@@ -4,6 +4,7 @@ class Mahasiswa(models.Model):
     _name = "mahasiswa"
     _description = "Mahasiswa"
 
+    image = fields.Binary()
     name = fields.Char(string="Nama")
     nim = fields.Char(string="NIM")
     tgl_sidang = fields.Date(string="Tgl Sidang")
